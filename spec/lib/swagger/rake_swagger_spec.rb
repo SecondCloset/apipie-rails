66f3217f37759d65860f0a2bd7dd83441b4e0bab
danny <kaiwen006@gmail.com>
2021-08-17 14:11:53 -0400

    remove params in: from title

--- spec/lib/swagger/rake_swagger_spec.rb
+++ spec/lib/swagger/rake_swagger_spec.rb
@@ -76,7 +76,7 @@ describe 'rake tasks' do
       it "generates static swagger files for the default version of apipie docs" do
         # print apidoc_swagger_json
 
-        expect(apidoc_swagger["info"]["title"]).to eq("Test app (params in:body)")
+        expect(apidoc_swagger["info"]["title"]).to eq("Test app")
         expect(apidoc_swagger["info"]["version"]).to eq("#{Apipie.configuration.default_version}")
       end
 
@@ -106,7 +106,7 @@ describe 'rake tasks' do
       it "generates static swagger files for the default version of apipie docs" do
         # print apidoc_swagger_json
 
-        expect(apidoc_swagger["info"]["title"]).to eq("Test app (params in:formData)")
+        expect(apidoc_swagger["info"]["title"]).to eq("Test app")
         expect(apidoc_swagger["info"]["version"]).to eq("#{Apipie.configuration.default_version}")
 
       end
